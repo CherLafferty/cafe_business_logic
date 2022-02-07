80b100f87764c8873e3e6bd26c53d889543daf40
Cher Lafferty <cherlafferty01@gmail.com>
2022-02-06 18:02:51 -0800

    initial

---
+++ CafeUtil.java
@@ -0,0 +1,37 @@
+
+public class CafeUtil {
+
+    public int getStreakGoal() {
+        int sum = 0;
+        int i = 0;
+        while(i<11){
+            sum = sum + i;
+            i++;
+        }
+        return sum;
+    }
+
+    // public double getOrderTotal(double[]prices){
+    //     // double[] prices = new prices[4];
+    //     double orderTotal = 0;
+    //     // double i;
+    //     for(double i = 0; i < prices.length; i++){
+    //         orderTotal += prices[i];
+    //     }
+    //     return orderTotal;
+    // }
+
+    // public void displayMenu(ArrayList<String>menuitems) {
+    //     String cafeList [];
+    //     for(string i = 0; i < menuitems.length; i++){
+    //         String cafeList = menuitems.get(i);
+    //         i++;
+    //     }
+    //     return cafeList;
+    // }
+
+    // public void addCutomer(ArrayList<String>customer){
+
+    // }
+
+}
